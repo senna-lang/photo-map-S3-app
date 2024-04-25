ab1fb7817723ca318d3c2a5c93c78ed261de8381
senna-lang <asrsena3302@gmail.com>
2024-04-25 13:13:10 +0900

    add types

--- src/common/types/types.ts
+++ src/common/types/types.ts
@@ -2,3 +2,15 @@ export type LngLat = {
   lng: number | null;
   lat: number | null;
 };
+export type Album =
+  | {
+      coordinate: {
+        lng: number;
+        lat: number;
+      } | null;
+      created_at: string;
+      id: number;
+      image_url: string | null;
+      user_id: string | null;
+    }[]
+  | null;
