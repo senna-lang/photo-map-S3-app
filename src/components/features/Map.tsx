ed9e3a9220155d35300a682d6059ce0df6214975
senna-lang <asrsena3302@gmail.com>
2024-04-25 13:13:33 +0900

    add database marker

--- src/components/features/Map.tsx
+++ src/components/features/Map.tsx
@@ -4,14 +4,16 @@ import Map from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { Marker, Popup } from 'react-map-gl';
 import { useState } from 'react';
-import { LngLat } from '@/common/types/types';
+import { Album, LngLat } from '@/common/types/types';
 
-const Mapbox = () => {
+const Mapbox = ({ album }: { album: Album }) => {
   const [lngLat, setLngLat] = useState<LngLat>({
     lng: null,
     lat: null,
   });
   const [showPopup, setShowPopup] = useState<boolean>(true);
+
+  // console.log(lngLat)
 
   const markedMap = (e: any) => {
     setLngLat({ ...lngLat, lng: e.lngLat.lng, lat: e.lngLat.lat });
@@ -30,24 +32,34 @@ const Mapbox = () => {
       mapStyle="mapbox://styles/senna-lang/clvaj709a00p901q13orl1h14"
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAP_BOX_ACCESS_KEY}
     >
-      <Marker
+      {album?.map(data => (
+        <Marker
+          longitude={data.coordinate!.lng}
+          latitude={data.coordinate!.lat}
+          anchor="bottom"
+          onClick={() => setShowPopup(true)}
+        />
+      ))}
+      {/* <Marker
         longitude={lngLat.lng!}
         latitude={lngLat.lat!}
         anchor="bottom"
-        onClick={() => setShowPopup(true)}
-      />
-      {/* {showPopup && (
-        <Popup
-          longitude={137.760725}
-          latitude={38.152981}
-          anchor="bottom"
-          onClose={() => setShowPopup(false)}
-        >
-          You are here
-        </Popup>
-      )} */}
+      /> */}
     </Map>
   );
 };
 
 export default Mapbox;
+
+{
+  /* {showPopup && (
+  <Popup
+    longitude={137.760725}
+    latitude={38.152981}
+    anchor="bottom"
+    onClose={() => setShowPopup(false)}
+  >
+    You are here
+  </Popup>
+)} */
+}
