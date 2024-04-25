61ee20b9e13d66942c631730c668e98dc1c0fc31
senna-lang <asrsena3302@gmail.com>
2024-04-25 20:47:54 +0900

    fixed type

--- src/common/types/types.ts
+++ src/common/types/types.ts
@@ -2,15 +2,13 @@ export type LngLat = {
   lng: number | null;
   lat: number | null;
 };
-export type Album =
-  | {
-      coordinate: {
-        lng: number;
-        lat: number;
-      } | null;
-      created_at: string;
-      id: number;
-      image_url: string | null;
-      user_id: string | null;
-    }[]
-  | null;
+export type Album = {
+  coordinate: {
+    lng: number;
+    lat: number;
+  } | null;
+  created_at: string;
+  id: number;
+  image_url: string | null;
+  user_id: string | null;
+};
