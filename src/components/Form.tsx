7126318a18e5ab3374d201120f3af8586a5912ca
senna-lang <asrsena3302@gmail.com>
2024-04-26 11:49:08 +0900

    add coordinate

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -11,9 +11,14 @@ import {
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { useImageForm } from '@/common/hooks/useForm';
+import { useStore } from '@/common/store/store';
 
 const ImageForm = () => {
   const { form, onSubmit } = useImageForm();
+  const { coordinate } = useStore();
+  const stringCoordinate = JSON.stringify(coordinate);
+  const { setValue } = form;
+  setValue('coordinate', stringCoordinate);
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -25,6 +30,23 @@ const ImageForm = () => {
               <FormLabel>File name</FormLabel>
               <FormControl>
                 <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="coordinate"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>座標</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="shadcn"
+                  {...field}
+                  value={stringCoordinate}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -51,6 +73,7 @@ const ImageForm = () => {
             </FormItem>
           )}
         />
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
