2da43c2f6873ba68d60aca8462e5799ea3767645
senna-lang <asrsena3302@gmail.com>
2024-04-26 17:48:55 +0900

    リファクタ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import { getAllAlbum, supabaseServer } from '@/common/lib/supabase';
-import ImageForm from '@/components/Form';
-import AuthButtonServer from '@/components/elements/AuthButtonServer';
 import Mapbox from '@/components/features/Map';
 
 export default async function Home() {
   const album = await getAllAlbum(supabaseServer());
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <AuthButtonServer />
-      <ImageForm />
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Mapbox album={album} />
     </main>
   );
