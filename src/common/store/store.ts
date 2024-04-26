785b46ae52ffc61b2d27c7bb6de39bcbf2cf5caf
senna-lang <asrsena3302@gmail.com>
2024-04-26 17:47:30 +0900

    add State

--- src/common/store/store.ts
+++ src/common/store/store.ts
@@ -2,11 +2,13 @@ import { create } from 'zustand';
 import { LngLat } from '../types/types';
 
 type State = {
-  coordinate: LngLat
+  coordinate: LngLat;
+  showPinnedMarker: boolean;
 };
 
 type Action = {
   updateCoordinate: (coordinate: State['coordinate']) => void;
+  updateShowPinnedMarker: (showPinnedMarker: State['showPinnedMarker']) => void;
 };
 
 export const useStore = create<Action & State>(set => ({
@@ -14,5 +16,8 @@ export const useStore = create<Action & State>(set => ({
     lng: null,
     lat: null,
   },
+  showPinnedMarker: false,
   updateCoordinate: newCoordinate => set(() => ({ coordinate: newCoordinate })),
+  updateShowPinnedMarker: newShowPinnedMarker =>
+    set(() => ({ showPinnedMarker: newShowPinnedMarker })),
 }));
