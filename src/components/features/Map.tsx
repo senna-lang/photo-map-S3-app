27bc721056ac41578485f1f61e23d3ddd77fda56
senna-lang <asrsena3302@gmail.com>
2024-04-26 11:49:40 +0900

    coordinateをstoreへ移動

--- src/components/features/Map.tsx
+++ src/components/features/Map.tsx
@@ -2,20 +2,14 @@
 
 import Map from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { useRef,useState } from 'react';
+import { useRef, useState } from 'react';
 import { Album, LngLat } from '@/common/types/types';
 import { Marker } from 'react-map-gl';
 import GeoMarker from '../elements/Marker';
+import { useStore } from '@/common/store/store';
 
 const Mapbox = ({ album }: { album: Album[] | null }) => {
-  const [lngLat, setLngLat] = useState<LngLat>({
-    lng: null,
-    lat: null,
-  });
-
-  const markedMap = (e: any) => {
-    setLngLat({ ...lngLat, lng: e.lngLat.lng, lat: e.lngLat.lat });
-  };
+  const { coordinate, updateCoordinate } = useStore();
 
   return (
     <Map
@@ -26,18 +20,18 @@ const Mapbox = ({ album }: { album: Album[] | null }) => {
         zoom: 5,
       }}
       style={{ width: '100%', height: '100vh' }}
-      onClick={e => markedMap(e)}
+      onClick={e => updateCoordinate({ lat: e.lngLat.lat, lng: e.lngLat.lng })}
       mapStyle="mapbox://styles/senna-lang/clvaj709a00p901q13orl1h14"
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAP_BOX_ACCESS_KEY}
     >
       {album?.map((data: Album) => (
         <GeoMarker data={data} key={data.id} />
       ))}
-      {/* <Marker
-        longitude={lngLat.lng!}
-        latitude={lngLat.lat!}
+      <Marker
+        longitude={coordinate.lng!}
+        latitude={coordinate.lat!}
         anchor="bottom"
-      /> */}
+      />
     </Map>
   );
 };
