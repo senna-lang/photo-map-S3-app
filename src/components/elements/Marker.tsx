31f0e401161986ac03e6e29a2c70e3c8a7f5e248
senna-lang <asrsena3302@gmail.com>
2024-04-28 21:40:38 +0900

    リファクタ

--- src/components/elements/Marker.tsx
+++ src/components/elements/Marker.tsx
@@ -2,7 +2,6 @@
 
 import { Album } from '@/common/types/types';
 import { useState } from 'react';
-import Image from 'next/image';
 import { Popup, Marker } from 'react-map-gl';
 import StandardImageList from '../layouts/ImageList';
 
@@ -19,7 +18,7 @@ const GeoMarker = ({ data }: { data: Album }) => {
       {showPopup && (
         <Popup
           offset={25}
-          maxWidth='auto'
+          maxWidth="auto"
           closeOnClick={false}
           longitude={data.coordinate!.lng}
           latitude={data.coordinate!.lat}
@@ -28,13 +27,7 @@ const GeoMarker = ({ data }: { data: Album }) => {
         >
           <div className=" h-auto w-auto">
             <h1>{data.created_at}</h1>
-            {/* <Image
-              height={3000}
-              width={3000}
-              src={data.image_url!}
-              alt={data.created_at}
-            /> */}
-            <StandardImageList images={data.image_url}/>
+            <StandardImageList images={data.image_url} />
           </div>
         </Popup>
       )}
--- src/components/layouts/ImageList.tsx
+++ src/components/layouts/ImageList.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Image from 'next/image';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 
@@ -7,12 +8,12 @@ export default function StandardImageList({ images }: { images: string[] }) {
     <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
       {images.map((image,index) => (
         <ImageListItem key={index}>
-          <img
-            srcSet={`${image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            src={`${image}?w=164&h=164&fit=crop&auto=format`}
-            alt={image}
-            loading="lazy"
-          />
+           <Image
+              height={3000}
+              width={3000}
+              src={image}
+              alt={image}
+            />
         </ImageListItem>
       ))}
     </ImageList>
